f5fd17334f5ca3c7f6971b0362f0ed35246bc9e7
M. Sarfal Khamidi <msarfal.2020@student.uny.ac.id>
2022-01-28 10:40:45 +0700

    starts with uppercase

---
+++ Strings/startswith_uppercase.py
@@ -0,0 +1,12 @@
+from tabnanny import check
+
+
+def startswith_uppercase(string):
+    if "A" <= string[0] <= "Z":
+        return True
+    else:
+        return False
+
+if __name__=="__main__":
+    result = startswith_uppercase(input("input string: "))
+    print("Starts with Uppercase?", result)
